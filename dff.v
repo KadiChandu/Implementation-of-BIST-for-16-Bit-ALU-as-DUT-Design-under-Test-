d0fbe84cf49ce44ce4598d1e1b9ef751f3cdbb3e
Kadi Chandu <chandukadi111@gmail.com>
2025-08-06 18:39:29 +0530

    Update dff.v

--- dff.v
+++ dff.v
@@ -1,11 +1,11 @@
-module dff(
-  input d ,
-  input clk,
-  output q , 
-  output q_bar );
-  wire x , y ;
-  nand A (x , d , clk);
-  nand B (y , x , clk);
-  nand C (q  , q_bar , x);
-  nand D (q_bar , q , y);
+module dff(input clk, input rst, input d, output q);
+    wire n1, n2, n3, n4, n5, n6, qn;
+    nand (n1, d, clk);
+    nand (n2, n1, clk);
+    nand (n3, n1, qn);
+    nand (n4, n2, n3);
+    nand (n5, rst, n4);   // reset path
+    nand (n6, rst, n3);   // reset path for qn
+    nand (q, n5, qn);
+    nand (qn, q, n6);
 endmodule
